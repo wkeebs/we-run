223cc8886f4d0853b6dd47703f101bcf687e3a1c
Will Keeble <will.a.keeble@gmail.com>
2024-02-22 22:14:06 +1100

    user activities retrieved and length of first page displayed

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import Typography from "@mui/material/Typography";
+import { Box } from "@mui/material";
+
+import { CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN } from "./utils/userData";
+
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [activities, setActivities] = useState({});
+
+  const callRefresh = `https://www.strava.com/oauth/token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&refresh_token=${REFRESH_TOKEN}&grant_type=refresh_token`;
+
+  // endpoint for read-all activities. temporary token is added in getActivities()
+  const callActivities = `https://www.strava.com/api/v3/athlete/activities?access_token=`;
+
+  // Use refresh token to get current access token
+  useEffect(() => {
+    fetch(callRefresh, {
+      method: "POST",
+    })
+      .then((res) => res.json())
+      .then((result) => getActivities(result.access_token));
+  }, [callRefresh]);
+
+
+
+  // use current access token to call all activities
+  const getActivities = (access) => {
+    // console.log(callActivities + access)
+    fetch(callActivities + access)
+      .then((res) => res.json())
+      .then(
+        (data) => setActivities(data),
+        setIsLoading(false)
+      )
+      .catch((e) => console.log(e));
+  };
+
+  if (isLoading)
+    return (
+      <Typography variant="h3" color="initial">
+        Loading ...
+      </Typography>
+    );
+
+  return (
+    <>
+      <Box>
+        <Typography variant="body1" color="initial">
+          {activities.length} activities
+        </Typography>
+      </Box>
+    </>
+  );
+}
+
+export default App;
---
+++ src/utils/userData.jsx
@@ -0,0 +1,5 @@
+export const CLIENT_ID = "104496";
+export const CLIENT_SECRET = "e930e354bff22a2f78dd52d42d114a10af9881db";
+export const REFRESH_TOKEN = "a323fc3780b990525d65fb058967369f434ed9d2";
+
+export const current_token = '016c5747bff748723b22f5d42fea80d47c4c5cbe';
